c10427cbbb181af57709f5d2c3daebc52c285023
otikojairus1 <otikojairus@gmail.com>
2023-04-23 14:14:34 +0300

    chore:component extraction

--- src/App.js
+++ src/App.js
@@ -1,56 +1,73 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import { useEffect, useState } from "react";
+import { BASE_URI } from "./BASE_URI";
+import axios from "axios";
+import CategoryListing from "./components/CategoryListing";
+import Catalogue from "./components/Catalogue";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  // pagestate
+  const [CataloguePage, setCataloguePage] = useState(false);
+  const [CategoryPage, setCategoryPage] = useState(true);
+
+  let fetchasync = () => {
+    axios
+      .get(`${BASE_URI}/get/categories`)
+      .then((res) => {
+        setCategories(res.data.categories);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  let createCategory = async () => {
+    try {
+      let res = await axios.post(`${BASE_URI}/add/category`, {
+        category_name: "Funiture",
+      });
+      setCategories((prev) => [
+        { category_name: res.data.created.category_name },
+        ...prev,
+      ]);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let changeComponent = () => {
+      setCataloguePage(true);
+      setCategoryPage(false);
+  }
+  useEffect(() => {
+    fetchasync();
+  }, []);
   return (
     <div className="grid grid-cols-12 bg-[#F5F5F5]">
       <div className="col-span-2 h-[100svh] bg-white">
         <Sidebar />
       </div>
       <div className="col-span-10 h-[100svh]">
-        <div className="h-[50rem] m-auto mt-5 relative rounded-md bg-white w-[60rem] ">
-          <div className="m-5 mt-5 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-              />
-            </svg>
-            <p className="ml-5 font-bold">Category</p>
-          </div>
-          <div className="h-[3rem] ml-[5rem] mt-2 w-[20rem] border flex px-2 items-center justify-between">
-            <p>Category Name</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6 text-[#3862CD]"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-              />
-            </svg>
-          </div>
-          <div className="absolute right-[10rem] h-[3rem] rounded-3xl items-center flex justify-center text-white w-[14rem] bg-blue-700">
-            Create New Category
-          </div>
-          <div className="absolute right-[10rem] top-[10rem] h-[3rem] rounded-3xl items-center flex justify-center text-white w-[14rem] bg-blue-700">
-            Stock Update Report
-          </div>
+        <div
+        // onClick={() => {
+        //   setCataloguePage(true);
+        //   setCategoryPage(false);
+        // }}
+        >
+          {CategoryPage && (
+            <CategoryListing
+              changeComponent={changeComponent}
+              categories={categories}
+              createCategory={createCategory}
+            />
+          )}
         </div>
+
+        <div onClick={() => {}}>{CataloguePage && <Catalogue />}</div>
       </div>
     </div>
   );
